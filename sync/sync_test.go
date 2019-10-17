b7558cd20c2c069e1460a60dbe816e747f4d9dfb
Rafael Valiev <rafael.valiev@messagebird.com>
2019-10-17 18:21:25 +0200

    Sync in concurrency

---
+++ sync/sync_test.go
@@ -0,0 +1,49 @@
+package sync
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("Increment the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := NewCounter()
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+		assertCounter(t, counter, 3)
+	})
+
+	t.Run("It runs safely in concurrent", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				(*counter).Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
+}
+
+func BenchmarkCounter(b *testing.B) {
+	counter := NewCounter()
+	for i := 0; i < b.N; i++ {
+		counter.Inc()
+	}
+}
+
+func assertCounter(t *testing.T, got *Counter, want int) {
+	t.Helper()
+
+	if (*got).Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}
